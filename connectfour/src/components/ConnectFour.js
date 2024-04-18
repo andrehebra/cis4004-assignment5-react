de59ff155e15edc40651dc9d986b377a89fc26f5
andrehebra <52555960+andrehebra@users.noreply.github.com>
2024-04-18 22:28:55 +0000

    working on final render function

--- connectfour/src/components/ConnectFour.js
+++ connectfour/src/components/ConnectFour.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './ConnectFour.css';
+//import './ConnectFour.css';
 
 class ConnectFour extends React.Component {
     constructor(props) {
@@ -23,13 +23,171 @@ class ConnectFour extends React.Component {
 
         this.setPiect(5, colValue);
 
-        this.setState((prevState) => {
-            currentPlayer = prevState.currentPlayer == 1 ? 2 : 1;
-        })
+        this.setState((prevState) => ({
+            currentPlayer: prevState.currentPlayer === 1 ? 2 : 1
+        }))
 
     }
 
     setPiece(startCount, colValue) {
+        let initialMatrix = this.state.initialMatrix;
+
+        const rows = document.querySelectorAll(".grid-row");
+
+        try {
+            if (initialMatrix[startCount][colValue] !== 0) {
+                startCount--;
+                this.setPiece(startCount, colValue);
+            } else {
+                const currentRow = Array.from(rows)[startCount].querySelectorAll(".grid-box");
+                currentRow[colValue].classList.add("filled");
+                currentRow[colValue].classList.add("player${`this.state.currentPlayer}`");
+                initialMatrix[startCount][colValue] = this.state.currentPlayer;
+
+                if (this.winCheck) {
+                    alert("Player " + this.state.currentPlayer + " wins!");
+                    return true;
+                }
+            }
+        } catch(e) {
+            alert("Column full, select again");
+        }
+
+        this.gameOverCheck();
+    }
+
+    winCheck() {
+        console.log("winCheck");
+
+        if (this.checkHorizontal() || this.checkVertical() || this.checkPositiveDiagonal() || this.checkNegativeDiagonal()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    checkHorizontal() {
+        let initialMatrix = this.state.initialMatrix;
+        let currentPlayer = this.state.currentPlayer;
+
+        for (let i = 0; i < initialMatrix.length; i++) {
+            let currentCount = 0;
+            let j = 0;
+    
+            while (j < initialMatrix[i].length) {
+                if (initialMatrix[i][j] === currentPlayer) {
+                    currentCount++;
+                } else if (currentCount < 4) {
+                    currentCount = 0;
+                }
+                j++;
+            }
+    
+            if (currentCount >= 4) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+
+    checkVertical() {
+        let initialMatrix = this.state.initialMatrix;
+        let currentPlayer = this.state.currentPlayer;
+
+        for (let i = 0; i < initialMatrix[0].length; i++) {
+            let currentCount = 0;
+            let j = 0;
+    
+            while (j < initialMatrix.length) {
+                if (initialMatrix[j][i] === currentPlayer) {
+                    currentCount++;
+                } else if (currentCount < 4) {
+                    currentCount = 0;
+                }
+                j++;
+            }
+    
+            if (currentCount >= 4) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+
+    checkPositiveDiagonal() {
+        let initialMatrix = this.state.initialMatrix;
+        let currentPlayer = this.state.currentPlayer;
+
+        for (let i = 0; i < initialMatrix.length - 3; i++) {
+            for (let j = 0; j < initialMatrix[0].length - 3; j++) {
+                if (initialMatrix[i][j] === currentPlayer && initialMatrix[i+1][j+1] === currentPlayer && initialMatrix[i+2][j+2] === currentPlayer && initialMatrix[i+3][j+3] === currentPlayer) {
+                    return true;
+                }
+            }
+        }
+    
+    
+        return false;
+
+    }
+
+    checkNegativeDiagonal() {
+        let initialMatrix = this.state.initialMatrix;
+        let currentPlayer = this.state.currentPlayer;
+
+
+        for (let i = 3; i < initialMatrix.length; i++) {
+            for (let j = 0; j < initialMatrix[0].length - 3; j++) {
+                if (initialMatrix[i][j] === currentPlayer && initialMatrix[i-1][j+1] === currentPlayer && initialMatrix[i-2][j+2] === currentPlayer && initialMatrix[i-3][j+3] === currentPlayer) {
+                    return true;
+                }
+            }
+        }
+    
+    
+        return false;
+    }
+
+    gameOverCheck() {
+        let initialMatrix = this.state.initialMatrix;
+        let currentPlayer = this.state.currentPlayer;
+
+        let count = 0;
+
+        for (const innerArray of initialMatrix) {
+            let innerCount = 0;
+            for (let i = 0; i < innerArray.length; i++) {
+                if (innerArray[i] !== 0) {
+                    innerCount++;
+                }
+            }
+
+            if (innerCount === innerArray.length) {
+                count++;
+            } else {
+                return false;
+            }
+        }
+
+        if (count === 6) {
+            alert("Game Over");
+            return true;
+        }
+
+
+        return false;
+    }
+
+    render() {
+        return (
+            <div class="wrapper">
+                <div class="container"></div>
+            </div>
+        )
         
     }
 }
+
+export default ConnectFour;
--- connectfour/src/index.js
+++ connectfour/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import {ConnectFour} from './components/ConnectFour'
+import ConnectFour from './components/ConnectFour'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
